8d9af8379413a93a7a4329f1d2785c3b4bd065ca
Alexandre-Porto <vfdiv3@gmail.com>
2020-01-30 14:21:45 -0300

    update

--- head_pose_estimator.py
+++ head_pose_estimator.py
@@ -46,13 +46,13 @@ camera_matrix = np.array(
                          [0, 0, 1]], dtype = "double"
                          )
  
-print "Camera Matrix :\n {0}".format(camera_matrix)
+print("Camera Matrix :\n {0}".format(camera_matrix))
  
 dist_coeffs = np.zeros((4,1)) # Assuming no lens distortion
 (success, rotation_vector, translation_vector) = cv2.solvePnP(model_points, image_points, camera_matrix, dist_coeffs, flags=cv2.CV_ITERATIVE)
  
-print "Rotation Vector:\n {0}".format(rotation_vector)
-print "Translation Vector:\n {0}".format(translation_vector)
+print("Rotation Vector:\n {0}".format(rotation_vector))
+print("Translation Vector:\n {0}".format(translation_vector))
  
  
 # Project a 3D point (0, 0, 1000.0) onto the image plane.
